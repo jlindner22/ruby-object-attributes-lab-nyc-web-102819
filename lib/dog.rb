53f2d29b21c6a30c89eb5b4b3ee561109b820109
Jen Lindner <jenlindner22@gmail.com>
2019-10-29 19:24:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog 
-  def initialize(name)
-    @name = name
-  end
+  # def initialize(name)
+  #   @name = name
+  # end
   
-  def name
-    @name
+  def name(name)
+    @name = name
   end
   
     def name=(name)
